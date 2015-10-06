1cd5d2ae63dcffdc1a2c0600eb3d51fb10a96565
lclc98 <liam.c14785@gmail.com>
2015-10-06 21:52:57 +1000

    Useless code that is broken

--- src/main/java/net/epoxide/mha/item/ItemHorseArmorFlying.java
+++ src/main/java/net/epoxide/mha/item/ItemHorseArmorFlying.java
@@ -1,10 +1,13 @@
 package net.epoxide.mha.item;
 
 import net.darkhax.bookshelf.items.ItemHorseArmor;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.passive.EntityHorse;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class ItemHorseArmorFlying extends ItemHorseArmor {
 
@@ -28,11 +31,35 @@ public class ItemHorseArmorFlying extends ItemHorseArmor {
     @Override
     public String getArmorTexture (EntityHorse horse, ItemStack stack) {
 
-        return "textures/entity/horse/armor/horse_armor_iron.png";
+        return "mha:textures/entity/horse/armor/horse_armor_grayscale.png";
     }
 
     @Override
     public void onArmorRendering (EntityHorse entity, ItemStack stack, RendererLivingEntity renderer, double posX, double posY, double posZ, int flag) {
 
+        ModelHorseOverlay overlay = new ModelHorseOverlay();
+
+        if (flag == 0) {
+            GL11.glPushMatrix();
+            float r = 180f / 255f;
+            float g = 144f / 255f;
+            float b = 90f / 255f;
+            GL11.glColor4f(r, g, b, 1f);
+            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(getArmorTexture(entity, stack)));
+            overlay.render(entity, (float) posX, (float) posY, (float) posZ, 0, 0, 0);
+            GL11.glPopMatrix();
+
+            GL11.glPushMatrix();
+            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("mha:textures/entity/horse/armor/horse_armor_oak.png"));
+            overlay.render(entity, (float) posX, (float) posY, (float) posZ, 0, 0, 0);
+
+            GL11.glPopMatrix();
+        }
+
+    }
+
+    private float getColor (int x) {
+
+        return x / 255;
     }
 }
---
+++ src/main/java/net/epoxide/mha/item/ModelHorseOverlay.java
@@ -0,0 +1,6 @@
+package net.epoxide.mha.item;
+
+import net.minecraft.client.model.ModelHorse;
+
+public class ModelHorseOverlay extends ModelHorse {
+}
