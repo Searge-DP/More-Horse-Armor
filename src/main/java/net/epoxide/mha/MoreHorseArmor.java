0d4f024ad626a530ef919b91991b4b4544e7fadf
Darkhax <darklime@live.ca>
2015-10-11 01:30:30 -0600

    Added code to show item ids. Remember to get rid of this before building!

--- src/main/java/net/epoxide/mha/MoreHorseArmor.java
+++ src/main/java/net/epoxide/mha/MoreHorseArmor.java
@@ -7,11 +7,15 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.epoxide.mha.addon.AddonBotania;
 import net.epoxide.mha.common.ProxyCommon;
 import net.epoxide.mha.handler.MHAConfigurationHandler;
 import net.epoxide.mha.item.ItemManager;
 import net.epoxide.mha.util.Constants;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, guiFactory = Constants.FACTORY, dependencies = "required-after:bookshelf;after:Botania")
 public class MoreHorseArmor {
@@ -27,8 +31,11 @@ public class MoreHorseArmor {
 
         if(Loader.isModLoaded("Botania"))
             new AddonBotania();
+        
         new ItemManager();
         new MHAConfigurationHandler(event.getSuggestedConfigurationFile());
+        
+        MinecraftForge.EVENT_BUS.register(this);
     }
     
     @EventHandler
@@ -39,5 +46,12 @@ public class MoreHorseArmor {
     @EventHandler
     public void postInit (FMLPostInitializationEvent event) {
     
+    }
+    
+    @SubscribeEvent
+    public void onTooltipDisplayed(ItemTooltipEvent event) {
+        
+        if (event.itemStack != null && event.itemStack.getItem() != null)
+            event.toolTip.add("Name: " + Item.itemRegistry.getNameForObject(event.itemStack.getItem()));
     }
 }
