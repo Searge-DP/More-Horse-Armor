98d85a0f74a32c5673cec0b1eaed5f362bbc4f98
Darkhax <darklime@live.ca>
2015-10-11 03:24:31 -0600

    Imports

--- src/main/java/net/epoxide/mha/MoreHorseArmor.java
+++ src/main/java/net/epoxide/mha/MoreHorseArmor.java
@@ -4,8 +4,6 @@ import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.epoxide.mha.addon.AddonBotania;
 import net.epoxide.mha.common.ProxyCommon;
