381bbffa49d5d2c5a95d7898f5350ea5097b5fa3
Darkhax <darklime@live.ca>
2015-10-11 02:14:31 -0600

    recipes are now damage sensitive.

--- src/main/java/net/epoxide/mha/item/ItemManager.java
+++ src/main/java/net/epoxide/mha/item/ItemManager.java
@@ -7,6 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
 public class ItemManager {
+    
     public static final Item itemHorseArmorFlying = addTierRecipe(new ItemHorseArmorFlying("flying", 11), "");
     public static final Item itemHorseArmorFlame = addTierRecipe(new ItemHorseArmorFlame("flame", 11), "");
     public static final Item itemHorseArmorNature = addTierRecipe(new ItemHorseArmorNature("nature", 11), "");
@@ -23,28 +24,9 @@ public class ItemManager {
      */
     public static void addTierRecipe (HorseArmorTier tier) {
 
-        Object material = Utilities.getThingByName(tier.getRecipeItemName());
-
+        ItemStack material = Utilities.createStackFromString(tier.getRecipeItemName());
+        
         if (tier.isEnabled() && material != null)
             GameRegistry.addShapedRecipe(new ItemStack(tier.getItem()), "  x", "xyx", "xxx", Character.valueOf('x'), material, Character.valueOf('y'), Items.leather);
-    }
-
-    public static Item addTierRecipe (ItemHorseArmorBase armorBase, String item) {
-
-        System.out.println(Item.itemRegistry.getKeys());
-        Object material = Utilities.getThingByName(item);
-
-        if (material != null)
-            GameRegistry.addShapedRecipe(new ItemStack(armorBase), "  x", "xyx", "xxx", Character.valueOf('x'), material, Character.valueOf('y'), Items.leather);
-
-        return armorBase;
-    }
-
-    public static Item addTierRecipe (ItemHorseArmorBase armorBase, ItemStack item) {
-
-        if (item != null)
-            GameRegistry.addShapedRecipe(new ItemStack(armorBase), "  x", "xyx", "xxx", Character.valueOf('x'), item, Character.valueOf('y'), Items.leather);
-
-        return armorBase;
     }
 }
